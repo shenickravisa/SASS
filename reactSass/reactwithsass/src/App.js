e6a5cd8ca67b87ea61a82a81d9a30e3d5a3980ba
shenickravisa <cualquiercosa>
2020-10-06 22:58:41 -0500

    SG: save current work

---
+++ reactSass/reactwithsass/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="container-text app-container-bg">
+        <h1>Hola mundo</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
