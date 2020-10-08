91f721c03629dedaae9e0d34d77a67fefe1047d5
shenickravisa <cualquiercosa>
2020-10-08 11:32:06 -0500

    SG: save current work

--- reactSass/reactwithsass/src/App.js
+++ reactSass/reactwithsass/src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './styles/App.scss';
 
 function App() {
+  //ejemplo de sass ejemplo 1
   return (
     <>
-      <div className="container-text app-container-bg">
-        <h1>Hola mundo</h1>
-      </div>
+      <section>
+        <h2>Botones</h2>
+        <input type="submit"></input>
+      </section>
+      <section>
+        <h2>Tipografía</h2>
+        <p>Lorem</p>
+      </section>
     </>
   );
 }
